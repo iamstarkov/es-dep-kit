24c2e2b56524f52c09e83b842541f307522e5c4b
Vladimir Starkov <iamstarkov@gmail.com>
2016-04-29 18:41:22 +0200

    implement thing

--- index.js
+++ index.js
@@ -1,15 +1,99 @@
-function esDepKitAsync(input) {
-  if (typeof input !== 'string') {
-    return Promise.reject(new TypeError(`\input\` should be \`String\`, got \`${typeof input}\``));
-  }
-  return Promise.resolve(input);
-}
+/* eslint-disable no-underscore-dangle */
+import R from 'ramda';
+import contract from 'neat-contract';
+import pathIsAbsolute from 'path-is-absolute';
+import pathIsRelative from 'is-relative-path';
 
-function esDepKit(input) {
-  if (typeof input !== 'string') {
-    throw new TypeError(`\input\` should be \`String\`, got \`${typeof input}\``);
-  }
-  return input;
-}
+// isNotNil :: a -> Boolean
+const isNotNil = R.complement(R.isNil);
 
-export { esDepKit, esDepKitAsync };
+// _isLocalFile :: String -> Boolean
+const _isLocalFile = R.pipe(
+  contract('path', String),
+  R.either(pathIsAbsolute, pathIsRelative)
+);
+
+// _isModule :: String -> Boolean
+const _isModule = R.pipe(
+  contract('path', String),
+  R.complement(_isLocalFile)
+);
+
+// _requested :: Object -> String
+const _requested = R.prop('requested');
+
+// _resolved :: Object -> String
+const _resolved = R.prop('resolved');
+
+// _from :: Object -> String
+const _from = R.prop('from');
+
+// requestedModule :: Object -> Boolean
+const isEntry = R.unary(R.pipe(
+  contract('dep', Object),
+  _requested,
+  R.isNil
+));
+
+// requestedModule :: Object -> Boolean
+const requestedModule = R.unary(R.pipe(
+  contract('dep', Object),
+  _requested,
+  R.ifElse(isNotNil, _isModule, R.F)
+));
+
+// requestedLocalFile :: Object -> Boolean
+const requestedLocalFile = R.unary(R.pipe(
+  contract('dep', Object),
+  _requested,
+  R.ifElse(isNotNil, _isLocalFile, R.F)
+));
+
+// inNodeModules :: Object -> Boolean
+const inNodeModules = R.unary(R.pipe(
+  contract('dep', Object),
+  _resolved,
+  R.ifElse(isNotNil, R.test(/node_modules/), R.F)
+));
+
+// requestedFromNodeModules :: Object -> Boolean
+const requestedFromNodeModules = R.unary(R.pipe(
+  contract('dep', Object),
+  _from,
+  R.ifElse(isNotNil, R.test(/node_modules/), R.F)
+));
+
+// resolved :: Object -> Boolean
+const resolved = R.unary(R.pipe(
+  contract('dep', Object),
+  _resolved,
+  isNotNil
+));
+
+// notResolved :: Object -> Boolean
+const notResolved = R.unary(R.pipe(
+  contract('dep', Object),
+  _resolved,
+  R.isNil
+));
+
+// isThirdParty :: Object -> Boolean
+const isThirdParty = R.cond([
+  [isEntry, R.F],
+  [requestedModule, requestedFromNodeModules],
+  [requestedLocalFile, R.either(requestedFromNodeModules, inNodeModules)],
+]);
+
+export default {
+  _requested,
+  _resolved,
+  _from,
+  isEntry,
+  requestedModule,
+  requestedLocalFile,
+  inNodeModules,
+  requestedFromNodeModules,
+  resolved,
+  notResolved,
+  isThirdParty,
+};
--- test.js
+++ test.js
@@ -1,15 +1,170 @@
+/* eslint-disable no-multi-spaces */
 import test from 'ava';
-import { esDepKit, esDepKitAsync } from './index';
+import { esDepUnit as dep } from 'es-dep-unit';
+import kit from './index';
 
-test('basic', t =>
-  t.is(esDepKit('unicorns'), 'unicorns'));
+const entry = dep(null, null, 'index.js');
 
-test('empty input', t => t.throws(() => { esDepKit(); }, TypeError));
-test('invalid input', t => t.throws(() => { esDepKit(2); }, TypeError));
+const file      = dep('./file',       'index.js', 'file.js');
+const fileExtra = dep('./file-extra', 'index.js', null);
 
-test('async :: basic', async t => t.is(
-  await esDepKitAsync('unicorns'),
-  'unicorns'));
+const folder      = dep('./folder',       'index.js', 'folder/index.js');
+const folderExtra = dep('./folder-extra', 'index.js', null);
 
-test('async :: empty input', t => t.throws(esDepKitAsync(), TypeError));
-test('async :: invalid input', t => t.throws(esDepKitAsync(2), TypeError));
+const pkg      = dep('pkg',       'index.js', 'node_modules/pkg/index.js');
+const pkgExtra = dep('pkg-extra', 'index.js', null);
+
+const pkgFile      = dep('./pkg-file',       'node_modules/pkg/index.js', 'node_modules/pkg/file.js'); // eslint-disable-line
+const pkgFileExtra = dep('./pkg-file-extra', 'node_modules/pkg/index.js', null);
+
+const nestedPkg      = dep('nested-pkg',       'node_modules/pkg/index.js', 'node_modules/pkg/node_modules/nested-pkg/index.js'); // eslint-disable-line
+const nestedPkgExtra = dep('nested-pkg-extra', 'node_modules/pkg/index.js', null);
+
+const nestedPkgFile      = dep('./nested-pkg-file',       'node_modules/pkg/node_modules/nested-pkg/index.js', 'node_modules/pkg/node_modules/nested-pkg/file.js'); // eslint-disable-line
+const nestedPkgFileExtra = dep('./nested-pkg-file-extra', 'node_modules/pkg/node_modules/nested-pkg/index.js', null); // eslint-disable-line
+
+const cases = [
+  entry,
+  file, fileExtra,
+  folder, folderExtra,
+  pkg, pkgExtra,
+  pkgFile, pkgFileExtra,
+  nestedPkg, nestedPkgExtra,
+  nestedPkgFile, nestedPkgFileExtra,
+];
+
+test('isEntry', t => {
+  const expected = [
+    true,         // entry
+    false, false, // file
+    false, false, // folder
+    false, false, // pkg
+    false, false, // pkgFile
+    false, false, // nestedPkg
+    false, false, // nestedPkgFile
+  ];
+  expected.forEach((item, i) => {
+    t.is(kit.isEntry(cases[i]), expected[i]);
+  });
+});
+test('isEntry: empty input', t => t.throws(() => { kit.isEntry(); }, TypeError));
+test('isEntry: invalid input', t => t.throws(() => { kit.isEntry(2); }, TypeError));
+
+test('requestedModule', t => {
+  const expected = [
+    false,        // entry
+    false, false, // file
+    false, false, // folder
+    true,  true,  // pkg
+    false, false, // pkgFile
+    true,  true,  // nestedPkg
+    false, false, // nestedPkgFile
+  ];
+  expected.forEach((item, i) => {
+    t.is(kit.requestedModule(cases[i]), expected[i]);
+  });
+});
+test('requestedModule: empty input', t => t.throws(() => { kit.requestedModule(); }, TypeError));
+test('requestedModule: invalid input', t => t.throws(() => { kit.requestedModule(2); }, TypeError));
+
+test('requestedLocalFile', t => {
+  const expected = [
+    false,        // entry
+    true,  true,  // file
+    true,  true,  // folder
+    false, false, // pkg
+    true,  true,  // pkgFile
+    false, false, // nestedPkg
+    true,  true,  // nestedPkgFile
+  ];
+  expected.forEach((item, i) => {
+    t.is(kit.requestedLocalFile(cases[i]), expected[i]);
+  });
+});
+test('requestedLocalFile: empty input', t => t.throws(() => { kit.requestedLocalFile(); }, TypeError));
+test('requestedLocalFile: invalid input', t => t.throws(() => { kit.requestedLocalFile(2); }, TypeError));
+
+test('inNodeModules', t => {
+  const expected = [
+    false,        // entry
+    false, false, // file
+    false, false, // folder
+    true,  false, // pkg
+    true,  false, // pkgFile
+    true,  false, // nestedPkg
+    true,  false, // nestedPkgFile
+  ];
+  expected.forEach((item, i) => {
+    t.is(kit.inNodeModules(cases[i]), expected[i]);
+  });
+});
+test('inNodeModules: empty input', t => t.throws(() => { kit.inNodeModules(); }, TypeError));
+test('inNodeModules: invalid input', t => t.throws(() => { kit.inNodeModules(2); }, TypeError));
+
+test('requestedFromNodeModules', t => {
+  const expected = [
+    false,        // entry
+    false, false, // file
+    false, false, // folder
+    false, false, // pkg
+    true,  true,  // pkgFile
+    true,  true,  // nestedPkg
+    true,  true,  // nestedPkgFile
+  ];
+  expected.forEach((item, i) => {
+    t.is(kit.requestedFromNodeModules(cases[i]), expected[i]);
+  });
+});
+test('requestedFromNodeModules: empty input', t => t.throws(() => { kit.requestedFromNodeModules(); }, TypeError)); // eslint-disable-line
+test('requestedFromNodeModules: invalid input', t => t.throws(() => { kit.requestedFromNodeModules(2); }, TypeError)); // eslint-disable-line
+
+test('resolved', t => {
+  const expected = [
+    true,       // entry
+    true, false, // file
+    true, false, // folder
+    true, false, // pkg
+    true, false, // pkgFile
+    true, false, // nestedPkg
+    true, false, // nestedPkgFile
+  ];
+  expected.forEach((item, i) => {
+    t.is(kit.resolved(cases[i]), expected[i]);
+  });
+});
+test('resolved: empty input', t => t.throws(() => { kit.resolved(); }, TypeError));
+test('resolved: invalid input', t => t.throws(() => { kit.resolved(2); }, TypeError));
+
+test('not resolved', t => {
+  const expected = [
+    false,       // entry
+    false, true, // file
+    false, true, // folder
+    false, true, // pkg
+    false, true, // pkgFile
+    false, true, // nestedPkg
+    false, true, // nestedPkgFile
+  ];
+  expected.forEach((item, i) => {
+    t.is(kit.notResolved(cases[i]), expected[i]);
+  });
+});
+test('notResolved: empty input', t => t.throws(() => { kit.notResolved(); }, TypeError));
+test('notResolved: invalid input', t => t.throws(() => { kit.notResolved(2); }, TypeError));
+
+test('isThirdParty', t => {
+  const expected = [
+    false,        // entry
+    false, false, // file
+    false, false, // folder
+    false, false, // pkg
+    true,  true,  // pkgFile
+    true,  true,  // nestedPkg
+    true,  true,  // nestedPkgFile
+  ];
+  expected.forEach((item, i) => {
+    t.is(kit.isThirdParty(cases[i]), expected[i]);
+  });
+});
+test('isThirdParty: empty input', t => t.throws(() => { kit.isThirdParty(); }, TypeError)); // eslint-disable-line
+test('isThirdParty: invalid input', t => t.throws(() => { kit.isThirdParty(2); }, TypeError)); // eslint-disable-line
